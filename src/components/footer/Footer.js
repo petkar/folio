0a042f8f3c203ecf526e68521ea5d799e9350ec3
Deepit Petkar <59476886+petkar@users.noreply.github.com>
2025-02-09 11:18:53 +0530

    Update homepage URL and footer year

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          2024{" "}
+          2025{" "}
           <a
             href="https://github.com/petkar"
             target="_blank"
